83279518b9438a49b6c367561dc59ac91199eb0e
tomas.alabes <tomas.alabes@f6244a2c-66a4-6c06-e57f-02f7fd6f9cc2>
2012-01-19 00:47:16 +0000

    Some performance improvements.

--- trunk/src/com/Client.java
+++ trunk/src/com/Client.java
@@ -9,22 +9,19 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.Socket;
 
-import com.utils.ImageUtils;
-
 import android.graphics.drawable.Drawable;
-import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
 
 public class Client extends Thread {
 
-	public static final String SERVER_IP = "10.0.2.15"; 
+	public static final String SERVER_IP = "10.0.2.15";
+	public static final String TCP_IP_ADDRESS = "10.0.2.2";
 	public static final int UDP_LISTENING_PORT = 5000;
 	public static final int TCP_COMMUNICATING_PORT = 4444;
 	private static final int MAX_UDP_PACKET_SIZE = 65507;
 	
-	private DatagramSocket socket;
 	private boolean receiving = false;
 	
 	private Handler uiHandler;
@@ -39,62 +36,51 @@ public class Client extends Thread {
 		Log.i("TCP", "S: Starting handshake...");
 		doTCPHandshake();
 		Log.i("TCP", "S: Handshake done!");
+		DatagramSocket socket = null;
 		try {
-			/* Retrieve the ServerName */
 			InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
-
-			Log.i("UDP", "S: Connecting...");
-			/* Create new UDP-Socket */
 			socket = new DatagramSocket(UDP_LISTENING_PORT, serverAddr);
 			receiving = true;
 			
 			int idx = 0;
-			int lost = 0;
+			//int lost = 0;
 			while(receiving){
 				byte[] data = new byte[MAX_UDP_PACKET_SIZE];
-				/* Prepare a UDP-Packet that can 
-				 * contain the data we want to receive */
 				DatagramPacket packet = new DatagramPacket(data, data.length);
-				Log.i("UDP", "Receiving...");
+				//Log.i("UDP", "Waiting for packet...");
 
-				/* Receive the UDP-Packet */
 				socket.receive(packet);
 				//Log.i("UDP", "Packet received");
-				//Log.d("UDP", "packet index "+ idx + " and time: " +System.currentTimeMillis());
+				Log.d("UDP", "packet index "+ idx + " and time: " +System.currentTimeMillis());
 				idx++;
-				
 
 				//data = ImageUtils.extractBytes(packet.getData());
 				final ByteArrayInputStream bais = new ByteArrayInputStream(packet.getData());
 				final DataInputStream dis = new DataInputStream(bais);
 				int imageIdx = dis.readInt();
 				
-				/*if(imageIdx < lastImageIdx){
-					lost++;
-					Log.d("UDP", "lost one! Total: " + lost);
+				if(imageIdx < lastImageIdx){
+					//lost++;
+					//Log.d("UDP", "lost one! Total: " + lost);
 					continue;
 				}else if(imageIdx > lastImageIdx){
 					lastImageIdx = imageIdx;
-				}*/
+				}
 				int xOffset = dis.readInt();
 				int yOffset = dis.readInt();
 				dis.read(data);
 				//Log.d("UDP", "data length: "+data.length);
 				final InputStream img = new ByteArrayInputStream(data);
 				
-				/* Create Drawable */
 				final Drawable drawable = Drawable.createFromStream(img, "StreamName"); 
-				
+				//final Bitmap drawable = BitmapFactory.decodeByteArray(data, 0, data.length);
 				/* Create message to be send to the UI Thread */
 				final Message message = new Message();
 				message.obj = drawable;
-				final Bundle bundle = new Bundle();
-				bundle.putInt("ImageIdx", imageIdx);
-				bundle.putInt("xOffset", xOffset);
-				bundle.putInt("yOffset", yOffset);
-				message.setData(bundle);
+				message.arg1 = xOffset;
+				message.arg2 = yOffset;
 				if (uiHandler != null) {
-					uiHandler.sendMessage(message); // sendMessage
+					uiHandler.sendMessage(message);
 					Log.i("Server", "Message sent to UI");
 				}
 				
@@ -102,7 +88,7 @@ public class Client extends Thread {
 		} catch (Exception e) {
 			Log.e("UDP", "Error", e);
 		}finally{
-			socket.close();
+			if(socket != null) socket.close();
 		}
 	}
 	
@@ -117,7 +103,7 @@ public class Client extends Thread {
 		Socket socket = null;
 		try {
 			Log.i("TCP", "Pinging TCP handshake in port: " + TCP_COMMUNICATING_PORT);
-			socket = new Socket(InetAddress.getByName("10.0.2.2"), TCP_COMMUNICATING_PORT);
+			socket = new Socket(InetAddress.getByName(TCP_IP_ADDRESS), TCP_COMMUNICATING_PORT);
 			Log.i("TCP", "Adress reached.");
 		} catch (IOException e) {
 			Log.e("TCP", "Error", e);
@@ -134,7 +120,7 @@ public class Client extends Thread {
 	private void closeUDPTransaction() {
 		Socket socket = null;
 		try {
-			socket = new Socket(InetAddress.getByName("10.0.2.2"), TCP_COMMUNICATING_PORT);
+			socket = new Socket(InetAddress.getByName(TCP_IP_ADDRESS), TCP_COMMUNICATING_PORT);
 			Log.i("TCP", "Closing Socket created.");
 		} catch (IOException e) {
 			Log.e("TCP", "Error", e);
@@ -149,4 +135,6 @@ public class Client extends Thread {
 	}
 	
 }
+
+
 
